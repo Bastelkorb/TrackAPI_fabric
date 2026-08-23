2e82525a49cfb2b48742934d1afa25b5f2d6c721
Bastelkorb <87268409+Bastelkorb@users.noreply.github.com>
2026-08-23 12:18:28 +0200

    feat: Port TrackAPI main class to Fabric (remove Forge @Mod annotation)

--- src/main/java/trackapi/TrackAPI.java
+++ src/main/java/trackapi/TrackAPI.java
@@ -1,10 +1,10 @@
 package trackapi;
 
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(modid = TrackAPI.MODID, version = TrackAPI.VERSION, acceptedMinecraftVersions = "[1.12,1.13)")
-public class TrackAPI
-{
+/**
+ * TrackAPI - A common track interface for Minecraft
+ * Fabric Edition for Minecraft 1.16.5
+ */
+public class TrackAPI {
     public static final String MODID = "trackapi";
     public static final String VERSION = "1.3";
 }
